ebc047bad9c9abeaa47dae6830938f53ee94d9fa
jeffs5 <jeffs5s@gmail.com>
2015-02-06 10:13:07 -0800

    Created Basic Skeleton

--- src/Sandbox.java
+++ src/Sandbox.java
@@ -1,9 +1,61 @@
+import java.io.File;
+import java.util.ArrayList;
+
 
 public class Sandbox {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static ArrayList<File> fileList;
+	private static int totalDataFiles;
 
+	public static void main(String[] args) {
+
+		fileList = new ArrayList<File>();
+		totalDataFiles = 0;
+
+		//set to restrict how many files to go through
+		//set to -1 for infinite
+		int filesToVisit = -1;
+
+		//populate list with starting seeds
+		fileList.add(new File("/Users/jeffs5s/Desktop/enron_mail_20110402"));
+
+
+
+		while(!fileList.isEmpty() && filesToVisit != 0)
+		{
+			if(shouldVisit())
+				visit();
+			//			System.out.println(filesToVisit);
+			filesToVisit--;
+		}
+		
+		System.out.println("The total number of data files is " + totalDataFiles);
+
+	}
+
+	private static boolean shouldVisit()
+	{
+		return true;
+	}
+
+	private static void visit()
+	{
+		File file = fileList.remove(0);
+		//		System.out.println(file);
+		totalDataFiles++;
+
+		if (file.isDirectory())
+		{
+			//if visited directory has files to go to, add those files to the list to later visit
+			for(File f: file.listFiles())
+				fileList.add(f);
+			System.out.println("Directory");
+		}
+
+		else if(file.isFile())
+		{
+			System.out.println("File");
+		}
 	}
 
 }
